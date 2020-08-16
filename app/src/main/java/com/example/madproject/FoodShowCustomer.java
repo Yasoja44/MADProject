a7d95b26adde56d2849079f9f02394cf870bada1
Yasoja44 <68411003+Yasoja44@users.noreply.github.com>
2020-08-16 19:19:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madproject/FoodShowCustomer.java
@@ -0,0 +1,14 @@
+package com.example.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodShowCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_show_customer);
+    }
+}
